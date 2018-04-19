11d4f41b99a9535bf61e20dc18253fa312d57384
eXifreXi <cedric.neukirchen@gmx.de>
2018-04-19 17:41:57 +0200

    [ADDED] Now filtering messages that seem to have the same content

--- events/onMessageUpdate.js
+++ events/onMessageUpdate.js
@@ -7,16 +7,19 @@ module.exports = (oldMessage, newMessage) => {
 
     const client = oldMessage.client;
 
-    // We need to make sure that not all actions are logged
-    client.getPermissionLevel(oldMessage).then(permissionLevel => {
-        if(permissionLevel < 2) {
-            embedSender.logMessage(oldMessage, 'User edited a message',
-                `__User__: <@${oldMessage.author.id}>\n` +
-                `__Created at__: ${oldMessage.createdAt}\n` +
-                `__Channel__ : ${oldMessage.channel.name}\n` +
-                `__Old Message__: ${oldMessage.content}\n` +
-                `__New Message__: ${newMessage.content}`
-            );
-        }
-    });
+    // It seems like links cause this to trigger even if they aren't being modified
+    if(oldMessage.content != newMessage.content) {
+        // We need to make sure that not all actions are logged
+        client.getPermissionLevel(oldMessage).then(permissionLevel => {
+            if(permissionLevel < 2) {
+                embedSender.logMessage(oldMessage, 'User edited a message',
+                    `__User__: <@${oldMessage.author.id}>\n` +
+                    `__Created at__: ${oldMessage.createdAt}\n` +
+                    `__Channel__ : ${oldMessage.channel.name}\n` +
+                    `__Old Message__: ${oldMessage.content}\n` +
+                    `__New Message__: ${newMessage.content}`
+                );
+            }
+        });
+    }
 };
